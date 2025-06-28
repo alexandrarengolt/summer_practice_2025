e032253380cab0281c26abbab084b0d255d0bc2c
alexandrarengolt <alikrengolt@gmail.com>
2025-06-28 13:16:03 +0600

    Решение task01&task01tests

--- task01/Class1.cs
+++ task01/Class1.cs
@@ -1,6 +1,23 @@
 ﻿namespace task01;
-
-public class Class1
+public static class StringExtensions
 {
+    public static bool IsPalindrome(this string input)
+    {
+         if (string.IsNullOrEmpty(input))
+            return false;
+            
+        List<char> formatted = new List<char>();
 
+        string lowerInput = input.ToLower();
+        
+        foreach (var a in lowerInput)
+        {
+            if (!Char.IsWhiteSpace(a) && !Char.IsPunctuation(a)) formatted.Add(a);
+        }
+
+        List<char> reverseOrder = new List<char>(formatted);
+        reverseOrder.Reverse();
+
+        return formatted.SequenceEqual(reverseOrder);
+    }
 }
