fbed4b57324f3dce2e03694a7593ae82ee54e6b7
alexandrarengolt <alikrengolt@gmail.com>
2025-07-02 23:50:42 +0600

    realization of task04 and task04tests

--- task04tests/UnitTest1.cs
+++ task04tests/UnitTest1.cs
@@ -1,10 +1,75 @@
-﻿namespace task04tests;
-
-public class UnitTest1
+﻿using task04;
+using Xunit;
+namespace task04tests
 {
-    [Fact]
-    public void Test1()
+    public class SpaceshipTests
     {
+        [Fact]
+        public void Cruiser_ShouldHaveCorrectStats()
+        {
+            ISpaceship cruiser = new Cruiser();
+            Assert.Equal(50, cruiser.Speed);
+            Assert.Equal(80, cruiser.FirePower);
+        }
 
+        [Fact]
+        public void Fighter_ShouldHaveCorrectStats()
+        {
+            ISpaceship cruiser = new Fighter();
+            Assert.Equal(100, cruiser.Speed);
+            Assert.Equal(30, cruiser.FirePower);
+        }
+
+        [Fact]
+        public void Fighter_ShouldBeFasterThanCruiser()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+            Assert.True(fighter.Speed > cruiser.Speed);
+        }
+
+        [Fact]
+        public void Cruiser_ShouldBeStrongerThanFighter()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+            Assert.True(fighter.FirePower < cruiser.FirePower);
+        }
+
+        [Fact]
+        public void Spaceships_CorrectMoveForward()
+        {
+            ISpaceship fighter = new Fighter();
+            ISpaceship cruiser = new Cruiser();
+
+            fighter.MoveForward();
+            cruiser.MoveForward();
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Spaceships_CorrectRotate()
+        {
+            ISpaceship fighter = new Fighter();
+            ISpaceship cruiser = new Cruiser();
+
+            fighter.Rotate(70);
+            cruiser.Rotate(70);
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Spaceships_CorrectFire()
+        {
+            ISpaceship fighter = new Fighter();
+            ISpaceship cruiser = new Cruiser();
+
+            fighter.Fire();
+            cruiser.Fire();
+
+            Assert.True(true);
+        }
     }
 }
