707ac8aa5e9a9f2a385f14ba33ee06906fc59ebe
alexandrarengolt <alikrengolt@gmail.com>
2025-07-05 22:55:29 +0600

    realization of task08 and task08tests

--- CommandRunner/Program.cs
+++ CommandRunner/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using CommandLib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dllAssembly = Assembly.LoadFrom("FileSystemCommands.dll");
+
+        var commandTypeSize = dllAssembly.GetType("FileSystemCommands.DirectorySizeCommand");
+        var sizeCommand = (ICommand)Activator.CreateInstance(commandTypeSize, new object[] { @"C:\Test" });
+        sizeCommand.Execute();
+
+        var commandTypeFind = dllAssembly.GetType("FileSystemCommands.FindFilesCommand");
+        var findCommand = (ICommand)Activator.CreateInstance(commandTypeFind, new object[] { @"C:\Test", "*.txt" });
+        findCommand.Execute();
+    }
+}
--- task08tests/UnitTest1.cs
+++ task08tests/UnitTest1.cs
@@ -1,10 +1,40 @@
-﻿namespace task08tests;
+﻿using System;
+using Xunit;
+using FileSystemCommands;
+namespace task08tests;
 
-public class UnitTest1
+public class FileSystemCommandsTests
 {
     [Fact]
-    public void Test1()
+    public void DirectorySizeCommand_ShouldCalculateSize()
     {
+        var tempDirectory = Path.Combine(Path.GetTempPath(), "MomentDir");
+        Directory.CreateDirectory(tempDirectory);
+        File.WriteAllText(Path.Combine(tempDirectory, "test1.txt"), "Hello");
+        File.WriteAllText(Path.Combine(tempDirectory, "test2.txt"), "World");
 
+        var sizeCalculator = new DirectorySizeCommand(tempDirectory);
+        sizeCalculator.Execute();
+
+        Assert.Equal(10, sizeCalculator.totalSize);
+        Directory.Delete(tempDirectory, true);
     }
+
+    [Fact]
+    public void FindFilesCommand_ShouldFindMatchingFiles()
+    {
+        var searchDirectory = Path.Combine(Path.GetTempPath(), "MomentDir");
+        Directory.CreateDirectory(searchDirectory);
+        File.WriteAllText(Path.Combine(searchDirectory, "file1.txt"), "Text1");
+        File.WriteAllText(Path.Combine(searchDirectory, "file2.log"), "Log");
+        File.WriteAllText(Path.Combine(searchDirectory, "file3.txt"), "Text2");
+
+        var fileFinder = new FindFilesCommand(searchDirectory, "*.txt");
+        fileFinder.Execute();
+
+        Assert.Equal(2, fileFinder.receivedFiles.Length);
+
+        Directory.Delete(searchDirectory, true);
+    }
+
 }
