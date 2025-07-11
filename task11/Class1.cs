13f51aa61f9834d21972569ccde7275ada9d233e
alexandrarengolt <alikrengolt@gmail.com>
2025-07-11 23:20:10 +0600

    add realization of task11 and task11tests

--- task11/Class1.cs
+++ task11/Class1.cs
@@ -1,6 +1,41 @@
-﻿namespace task11;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
 
-public class Class1
+namespace task11;
+
+public interface ICalculator
 {
+    int Addition(int a, int b);
+    int Subtraction(int a, int b);
+    int Multiplication(int a, int b);
+    int Div(int a, int b);
+}
 
+[Generator]
+public class CalculationGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        string generated_code = @"
+namespace Generated
+{
+    public class Calculator : task11.ICalculator
+    {
+        public int Addition(int a, int b) => a + b;
+        public int Subtraction(int a, int b) => a - b;
+        public int Multiplication(int a, int b) => a * b;
+        public int Div(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}";
+
+        context.AddSource("GeneratedCalculator", SourceText.From(generated_code, Encoding.UTF8));
+    }
+
+    public void Initialize(GeneratorInitializationContext context) { }
 }
--- task11tests/UnitTest1.cs
+++ task11tests/UnitTest1.cs
@@ -1,10 +1,45 @@
-﻿namespace task11tests;
+﻿using Xunit;
+using task11;
 
-public class UnitTest1
+namespace task11tests
 {
-    [Fact]
-    public void Test1()
+    public class OperationsTests
     {
+        private readonly ICalculator _calculator;
 
+        public OperationsTests()
+        {
+            _calculator = new Generated.Calculator();
+        }
+
+        [Fact]
+        public void TesingAddition()
+        {
+            Assert.Equal(31, _calculator.Addition(30, 1));
+        }
+
+        [Fact]
+        public void TestingSubtraction()
+        {
+            Assert.Equal(44, _calculator.Subtraction(67, 23));
+        }
+
+        [Fact]
+        public void TesingMultiplication()
+        {
+            Assert.Equal(45, _calculator.Multiplication(5, 9));
+        }
+
+        [Fact]
+        public void TestingDiv()
+        {
+            Assert.Equal(11, _calculator.Div(22, 2));
+        }
+
+        [Fact]
+        public void WhenDividingByZero_ShouldThrowDivideByZeroException()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calculator.Div(100, 0));
+        }
     }
 }
